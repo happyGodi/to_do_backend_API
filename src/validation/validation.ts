0ca51e3f5a6d6a586183c947ce282acd1ef5c4d6
happyGodi <godiramaru0@gmail.com>
2023-03-29 19:41:07 +0100

    feat: form validation

---
+++ src/validation/validation.ts
@@ -0,0 +1,31 @@
+import joi from 'joi'
+
+const userSignValidator = (data: object) => {
+    const schema = joi.object().keys({
+        name: joi.string().required(),
+        username: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    })
+
+    return schema.validate(data)
+}
+const userLogValidator = (data: object) => {
+    const schema = joi.object().keys({
+        username: joi.string().required(),
+        password: joi.string().required()
+    })
+
+    return schema.validate(data)
+}
+
+const taskValidator = (data: object) => {
+    const schema = joi.object().keys({
+        name: joi.string().required(),
+        isDone: joi.boolean().required()
+    })
+
+    return schema.validate(data)
+}
+
+export  { userSignValidator, taskValidator, userLogValidator }
