ef0255ab714e342868de3ad2b1b43c9ec673f366
happyGodi <godiramaru0@gmail.com>
2023-03-29 22:03:34 +0100

    feat: delete method

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -32,7 +32,18 @@ const tasksController = (url: string) => {
             const alltasks = await Tasks.find();   
             res.status(200).json(alltasks);
         } catch (e) {
-            console.log(e)
+            res.json(e)
+        }
+    })
+    app.delete(url + '/delete/:id', auth, async (req: Request, res: Response) => {
+        try {
+            await Tasks.findByIdAndDelete(req.params.id)
+            let user = await users.findOne({ tasks: req.params.id})
+            user?.tasks.remove(req.params.id)
+            await user?.save()
+            return res.status(200).json({ message: 'Task deleted successfully' })
+        } catch (e) {
+            res.json(e)
         }
     })
 }
