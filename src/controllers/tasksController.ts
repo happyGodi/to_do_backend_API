5a6d7cdb98a35833636c167f0ccb2b3e2ac0ce82
happyGodi <godiramaru0@gmail.com>
2023-03-30 13:28:17 +0300

    feat: update method

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express"
 import users from "../models/usersModel"
 import Tasks from "../models/tasksModel"
 import { JwtPayload } from 'jsonwebtoken';
+import { taskValidator } from "../validation/validation"
 
 
 const tasksController = (url: string) => {
@@ -12,18 +13,23 @@ const tasksController = (url: string) => {
             let payload = (req as CustomRequest).token as JwtPayload
             let user = await users.findById(payload._id)
             if (!user) return res.status(400).json({ message: 'Acces denied!'})
+
+            const {error} = taskValidator(req.body);
+            if (error) return res.status(400).send(error.details[0].message);
+
             let task = new Tasks({
                 taskName: req.body.taskName,
                 date: Date.now(),
                 isDone: false,
                 user: user?._id
             })
+
             await task.save()
             user?.tasks.push(task)
             await user?.save()
             return res.status(200).json(task)
         } catch (e) {
-            res.json(e)
+            return res.json(e)
         }
     })
 
@@ -36,7 +42,7 @@ const tasksController = (url: string) => {
             const alltasks = await Tasks.find({ user: user._id});   
             res.status(200).json(alltasks);
         } catch (e) {
-            res.json(e)
+            return res.json(e)
         }
     })
     app.delete(url + '/delete/:id', auth, async (req: Request, res: Response) => {
@@ -47,7 +53,19 @@ const tasksController = (url: string) => {
             await user?.save()
             return res.status(200).json({ message: 'Task deleted successfully' })
         } catch (e) {
-            res.json(e)
+            return res.json(e)
+        }
+    })
+    app.put(url + '/update/:id', auth, async (req: Request, res: Response) => {
+        try {
+
+            const task = await Tasks.findById(req.params.id)
+            if (!task) return res.status(404).json({ message: 'Task not found' })
+            
+            await Tasks.findByIdAndUpdate(req.params.id, req.body, { new: true })
+            return res.status(200).json(task)
+        } catch (e) {
+            return res.json(e)
         }
     })
 }
